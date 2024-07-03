4fd8f946e95904b1f30d3647e7e1621607d0cf4b
Sam Paech <152407511+sqrkl@users.noreply.github.com>
2024-07-04 05:31:17 +1000

    change to IRT dataset

--- run_openai.py
+++ run_openai.py
@@ -62,7 +62,7 @@ def get_completion(prompt: str):
 
 
 def load_mmlu_pro():
-	dataset = load_dataset("TIGER-Lab/MMLU-Pro")
+	dataset = load_dataset("sam-paech/mmlu-pro-irt-1-0")
 	test_df, val_df = dataset["test"], dataset["validation"]
 	test_df = preprocess(test_df)
 	val_df = preprocess(val_df)
